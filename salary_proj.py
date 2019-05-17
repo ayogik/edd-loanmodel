01035cd54b06fc09cd81d4be96d4ec879310e2ef
ayogik <aykoppol@gmail.com>
2019-05-16 23:30:48 -0400

    Rename File, Update Salary Projection Function

--- salary_proj.py
+++ salary_proj.py
@@ -1,7 +1,16 @@
 from sympy.solvers import solve
 from sympy import Symbol
 
-def salary_proj(avg):
+variations = {'White':{'Male':1.11945,'Female':0.922821},
+              'African American':{'Male':0.838333,'Female':0.756954},
+              'Asian':{'Male':1.428603,'Female':1.102105},
+              'Hispanic':{'Male':0.796935,'Female':0.704974},
+              'Other':{'Male':1,'Female':0.8}}
+
+mort_rates = [5.367, 4.821, 4.391, 4.177, 4.000, 3.778]
+rate_range = [4.07, 13.25] #fixed rate
+
+def salary_proj_old(avg):
     r = 0.004
     multiplier = 1.1 
     start = 0.7*avg
@@ -13,14 +22,22 @@ def salary_proj(avg):
     
     foravg = 0.0
     for i in range(4,14):
-        foravg.append(out[i+1]-out[i])
+        foravg+=(out[i+1]-out[i])
     foravg/=10
     
     return [out, foravg]
+    
+def salary_proj(avg, gender, race, term=10):
+    
+    career = []
+    coef = variations[race][gender]
+    for i in range(0, 30):
+        career.append(avg*coef)
+    for i in range(0, 5):
+        career[i]*=(510.5714/873.0476)
+    return career
 
-
-mort_rates = [5.367, 4.821, 4.391, 4.177, 4.000, 3.778]
-rate_range = [4.07, 13.25] #fixed rate
+print(salary_proj(60000, 'Male', 'White'))
 
 def pred_interest(creditscore):
     if creditscore >= 620 and creditscore < 780:
@@ -35,7 +52,6 @@ def pred_interest(creditscore):
     return rate
     
 def recur(n, principal, const, monthly):
-
     if n == 1:
         return (principal*const - monthly)
     elif n>1:
@@ -46,6 +62,5 @@ def monthly_payment(principal, rate, term=10):
     monthly = Symbol('monthly')
     const = (1 + rate/365)**(365.0/12)
     out = solve(recur(term*12, principal, const, monthly), monthly)[0]
-    print(out)
-    print(out*12*term)
+    return [out, out*12*term - principal, out*12*term]
         
